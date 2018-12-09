97209aab1ea7854de470a21c146c5ee53cbe6fa6
pythonCat <pythoncat@qq.com>
2018-12-09 18:26:41 +0800

    @retetion 使用 (自定义注解一般使用 RetentionPolicy.RUNTIME ,然后可以被解析)

--- src/main/java/com/intellij/cat/annotation/Type.java
+++ src/main/java/com/intellij/cat/annotation/Type.java
@@ -1,11 +1,14 @@
 package com.intellij.cat.annotation;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
  * 自动注解：target -> class , method , local_var
  */
 @Target(value = {ElementType.TYPE, ElementType.METHOD, ElementType.LOCAL_VARIABLE})
+@Retention(value = RetentionPolicy.RUNTIME)
 public @interface Type {
 }
