dedd21fb35c9c8b52404073b2ff3bedeb53f2961
pythonCat <pythoncat@qq.com>
2018-12-13 09:42:49 +0800

    java 执行 js 脚本

--- src/main/java/com/intellij/cat/script/ScriptDemo.java
+++ src/main/java/com/intellij/cat/script/ScriptDemo.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class ScriptManager {
+public class ScriptDemo {
 
     public static void main(String[] args) throws ScriptException, NoSuchMethodException, FileNotFoundException {
 
